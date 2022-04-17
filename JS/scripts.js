e23706b2bd82a40c92502ac42352966681ce64de
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-04-17 10:54:36 -0300

    Implementação da busca de mensagens do servidor e aplicando estilos de acordo com o tipo da mensagem. A cada 3 segundos as mensagens são carregadas. Além disso, foi implementado o chat com rolagem automática

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,6 +2,8 @@ let nomeUsuario=prompt("Qual seu nome?");
 const objetoUsuario={
     name:nomeUsuario
 }
+const elementoMensagens=document.querySelector(".container");
+enviarSolicitacaoNome();
 function enviarSolicitacaoNome(){
     const promise=axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",objetoUsuario);
     promise.then(tratarSucessoNome);
@@ -9,24 +11,78 @@ function enviarSolicitacaoNome(){
 }
 function tratarErroNome(erro){
     let statusCode = erro.response.status;
-    if(statusCode!==200){
+    if(statusCode!==200 && statusCode===400){
         nomeUsuario=prompt("Nome já está sendo usado. Digite outro nome");
         objetoUsuario.name=nomeUsuario;
+        enviarSolicitacaoNome();
+    }else{
+        alert(`Erro ao enviar status de ${nomeUsuario} ao servidor: ${statusCode}`);
+        nomeUsuario=prompt("Qual seu nome?");
         enviarSolicitacaoNome();
     }
 }
 function tratarSucessoNome(resposta){
     let statusCode=resposta.status;
     if(statusCode===200){
-        setInterval(verificarStatus,5000)
+        setInterval(manterConexao,5000);
+        setInterval(buscarMensagens,3000);
+        
     }
 }
-function verificarStatus(){
+function manterConexao(){
     const promise=axios.post("https://mock-api.driven.com.br/api/v6/uol/status",objetoUsuario);
     promise.then(tratarSucessoStatus);
     promise.catch(tratarErroStatus);
 }
 function tratarSucessoStatus(resposta){
-    console.log(resposta.data);
+    console.log(`Servidor atualizou o status de ${nomeUsuario}`);
 }
-enviarSolicitacaoNome();
+function tratarErroStatus(erro){
+    alert(`Erro ao enviar status de ${nomeUsuario} ao servidor: ${erro.response.status}`);
+}
+function buscarMensagens(){
+    const promise=axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(tratarSucessoBuscarMensagem);
+    promise.catch(tratarErroBuscarMensagem);
+}
+function tratarSucessoBuscarMensagem(resposta){
+    const qntMensagens=resposta.data.length;
+    elementoMensagens.innerHTML=``;
+    let elementoMensagem;
+    for (let i=0;i<qntMensagens;i++){
+        if(resposta.data[i].type==="status"){
+            elementoMensagens.innerHTML+=
+            `<div id="${i}" class="mensagem status">
+                <h3><span>(${resposta.data[i].time})</span> <strong>${resposta.data[i].from}</strong> ${resposta.data[i].text}</h3>
+            </div>`;
+            elementoMensagem=document.getElementById(`${i}`)
+            elementoMensagem.scrollIntoView();
+        }
+        if(resposta.data[i].type==="message" && resposta.data[i].to==="Todos"){
+            elementoMensagens.innerHTML+=
+            `<div id="${i}" class='mensagem normais'>
+                <h3><span>(${resposta.data[i].time})</span> <strong>${resposta.data[i].from}</strong> para ${resposta.data[i].to}: ${resposta.data[i].text}</h3>
+            </div>`
+            elementoMensagem=document.getElementById(`${i}`)
+            elementoMensagem.scrollIntoView();
+        }
+        if(verificarMensagemPrivada(resposta.data[i].type,resposta.data[i].to,nomeUsuario)){
+            elementoMensagens.innerHTML+=
+            `<div id="${i}" class='mensagem reservada'>
+                <h3><span>(${resposta.data[i].time})</span> <strong>${resposta.data[i].from}</strong>       reservadamente para ${resposta.data[i].to}: ${resposta.data[i].text}</h3>
+            </div>`;
+            elementoMensagem=document.getElementById(`${i}`)
+            elementoMensagem.scrollIntoView();
+        }  
+    }
+    console.log(`site carregou as mensagens do servidor`);
+}
+function verificarMensagemPrivada(tipoDaMensagem,destinatario,nome){
+    if(tipoDaMensagem==="message" && destinatario===nome){
+        return true
+    }
+    return false;
+}
+function tratarErroBuscarMensagem(erro){
+    alert(`Erro ao receber as mensagem do servidor: ${erro.response.status}`);
+}
