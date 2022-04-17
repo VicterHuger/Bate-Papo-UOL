187e5c1fd3c4c2dbeb78236600cfec9d4e54b502
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-04-17 01:26:33 -0300

    Estabelecimento de conexão com o servidor, verificando o status do usuário a cada 5s-Manter conexão

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,36 +2,31 @@ let nomeUsuario=prompt("Qual seu nome?");
 const objetoUsuario={
     name:nomeUsuario
 }
-function enviarSolicitacao(){
+function enviarSolicitacaoNome(){
     const promise=axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",objetoUsuario);
-    promise.then(tratarSucesso);
-    promise.catch(tratarErro);
+    promise.then(tratarSucessoNome);
+    promise.catch(tratarErroNome);
 }
-function tratarErro(erro){
+function tratarErroNome(erro){
     let statusCode = erro.response.status;
-    if(statusCode===400){
+    if(statusCode!==200){
         nomeUsuario=prompt("Nome já está sendo usado. Digite outro nome");
         objetoUsuario.name=nomeUsuario;
-        enviarSolicitacao();
+        enviarSolicitacaoNome();
     }
 }
-function tratarSucesso(resposta){
+function tratarSucessoNome(resposta){
     let statusCode=resposta.status;
     if(statusCode===200){
-        console.log("Deu certo, nome pode ser usado")
+        setInterval(verificarStatus,5000)
     }
 }
-enviarSolicitacao();
-
-function verificarNome(nome,nomes){
-    const qntNomes=nomes.length;
-    let contador=0;//verificador de nomes disponíveis no servidor
-    for (let i=0;i<qntNomes;i++){
-        if(nome===nomes[i].name){
-            contador++; 
-            return contador;
-        }
-    }
-    return contador;
-
+function verificarStatus(){
+    const promise=axios.post("https://mock-api.driven.com.br/api/v6/uol/status",objetoUsuario);
+    promise.then(tratarSucessoStatus);
+    promise.catch(tratarErroStatus);
 }
+function tratarSucessoStatus(resposta){
+    console.log(resposta.data);
+}
+enviarSolicitacaoNome();
