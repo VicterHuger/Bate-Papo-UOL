84882993499dfd97b81d7e70f7e66e605ba8bfa2
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-04-17 12:02:00 -0300

    Projeto bruto finalizado - Funcionalidade de enviar mensagem para todos usuários e para determinado usuário foi implementada. Mensagens e status são atualizados automaticamente.

--- JS/scripts.js
+++ JS/scripts.js
@@ -10,7 +10,7 @@ function enviarSolicitacaoNome(){
     promise.catch(tratarErroNome);
 }
 function tratarErroNome(erro){
-    let statusCode = erro.response.status;
+    let statusCode = erro.response.data;
     if(statusCode!==200 && statusCode===400){
         nomeUsuario=prompt("Nome já está sendo usado. Digite outro nome");
         objetoUsuario.name=nomeUsuario;
@@ -38,7 +38,7 @@ function tratarSucessoStatus(resposta){
     console.log(`Servidor atualizou o status de ${nomeUsuario}`);
 }
 function tratarErroStatus(erro){
-    alert(`Erro ao enviar status de ${nomeUsuario} ao servidor: ${erro.response.status}`);
+    alert(`Erro ao enviar status de ${nomeUsuario} ao servidor: ${erro.response.data}`);
 }
 function buscarMensagens(){
     const promise=axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -66,7 +66,7 @@ function tratarSucessoBuscarMensagem(resposta){
             elementoMensagem=document.getElementById(`${i}`)
             elementoMensagem.scrollIntoView();
         }
-        if(verificarMensagemPrivada(resposta.data[i].type,resposta.data[i].to,nomeUsuario)){
+        if(verificarMensagemPrivada(resposta.data[i],nomeUsuario)){
             elementoMensagens.innerHTML+=
             `<div id="${i}" class='mensagem reservada'>
                 <h3><span>(${resposta.data[i].time})</span> <strong>${resposta.data[i].from}</strong>       reservadamente para ${resposta.data[i].to}: ${resposta.data[i].text}</h3>
@@ -77,12 +77,35 @@ function tratarSucessoBuscarMensagem(resposta){
     }
     console.log(`site carregou as mensagens do servidor`);
 }
-function verificarMensagemPrivada(tipoDaMensagem,destinatario,nome){
-    if(tipoDaMensagem==="message" && destinatario===nome){
+function verificarMensagemPrivada(dados,nome){
+    let destinatario=dados.to;
+    let origem=dados.from;
+    let tipoMensagem=dados.type;
+    if((destinatario===nome || origem===nome) && tipoMensagem==="private_message"){
         return true
     }
     return false;
 }
 function tratarErroBuscarMensagem(erro){
-    alert(`Erro ao receber as mensagem do servidor: ${erro.response.status}`);
+    alert(`Erro ao receber as mensagem do servidor: ${erro.response.data}`);
+}
+
+function enviarMensagem(){
+    const objetoMensagem={
+        from:nomeUsuario,
+        to:"Todos",//Jeferson
+        text: document.querySelector("input").value,
+        type: "message" // ou "private_message" para o bônus
+    }
+    const promise=axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",objetoMensagem);
+    promise.then(tratarSucessoEnviarMensagem);
+    promise.catch(tratarErroEnviarMensagem);
+    document.querySelector("input").value="";
+}
+function tratarSucessoEnviarMensagem(resposta){
+    buscarMensagens();
+}
+function tratarErroEnviarMensagem(erro){
+    alert(`Erro ao receber ao enviar mensagem para o servidor, usuário não está online: ${erro.response.data}`);
+    window.location.reload();
 }
